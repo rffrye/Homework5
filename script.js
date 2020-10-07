a7f0869340b054aaacdb7286d5a9d4ca839b2049
RFFRYE <rffrye@users.noreply.github.com>
2020-10-06 22:44:43 -0400

    document.ready on JS file,  added the date to the header using documentation from momentjs.com

---
+++ script.js
@@ -0,0 +1,12 @@
+//Makes sure the html has fully rendered to run JQuery
+$(document).ready(function () {
+
+    //  ADD CURRENT DATE AT THE TOP OF THE PAGE
+
+    //Formatting found using https://momentjs.com/docs/
+    var currentDay = moment().format("dddd, MMMM Do");
+    // set the text to current day targeting id = currentDay
+    $("#currentDay").text(currentDay);
+
+
+});

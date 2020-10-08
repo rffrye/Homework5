19c2158b4bb22ccc2dc18491cd9ea904ae971193
RFFRYE <rffrye@users.noreply.github.com>
2020-10-08 18:23:55 -0400

    added data attributes to each line of the planner to use later to determine the color of the bar.

--- script.js
+++ script.js
@@ -8,5 +8,18 @@ $(document).ready(function () {
     // set the text to current day targeting id = currentDay
     $("#currentDay").text(currentDay);
 
+    //  DEFINE VARIABLES
 
+    // HH format for a 24hr clock
+    var currentTime = moment().format("HH");
+     // parse the current time so we can compare it with another number later
+    var currentTimeInt = parseInt(currentTime);
+    var saveBtn = $(".saveBtn");
+
+
+
+
+
+
+    
 });
